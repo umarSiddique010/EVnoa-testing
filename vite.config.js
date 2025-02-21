d5fa8f480f12f98e472efb4184de79b7a0bb28ed
Md Umar Siddique <us70763@gmail.com>
2025-02-22 03:04:28 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-   base: '/your-repo-name/',
+   base: '/EVnoa-testing',
   optimizeDeps: {
     include: ['@popperjs/core']
   }
